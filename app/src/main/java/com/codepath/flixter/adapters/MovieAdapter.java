75c0b74c9c4ba635abea492a010e14251ecf2133
Tripp Sautner <trippsautner@gmail.com>
2021-02-16 20:20:07 -0500

    Added Landscape Orientation adjustments

--- app/src/main/java/com/codepath/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/flixter/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.flixter.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -66,7 +67,15 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageURL;
+
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                imageURL = movie.getBackdropPath();
+            } else {
+                imageURL = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageURL).into(ivPoster);
         }
     }
 }
